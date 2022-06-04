99c4a56655c93007c2dc18a5737947c53aba1a4d
07CCC <104282617+07CCC@users.noreply.github.com>
2022-06-04 17:20:54 -0500

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(".aliados-slideshow").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: false,
+});
+
+$(".cursos-slideshow").slick({
+    slidesToScroll: 1,
+    prevArrow: '.arrow-left',
+    nextArrow: '.arrow-right',
+});
+
+sal();
